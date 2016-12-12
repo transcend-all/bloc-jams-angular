4e214c71f287692efc8101d9b3fc4f3140d3090f
transcend-all <transcendentalguy@gmail.com>
2016-12-12 17:56:43 -0500

    checkpoint 5 complete

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors', 
+    artist: 'Pablo Picasso', 
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '161.71', audioUrl: 'assets/music/blue'},
+        {title: 'Green', duration: '103.96', audioUrl: 'assets/music/green'},
+        {title: 'Red', duration: '268.45', audioUrl: 'assets/music/red'},
+        {title: 'Pink', duration: '153.14', audioUrl: 'assets/music/pink'},
+        {title: 'Magenta', duration: '374.22', audioUrl: 'assets/music/magenta'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Gugliermo Marconi',
+    label: 'EM',
+    year: '1989',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {title: 'Hello, Operator?', duration: '1:01'},
+        {title: 'Ring, ring, ring', duration: '5:01'},
+        {title: 'Fits in your pocket' , duration: '3:21'},
+        {title: 'Can you hear me now?' , duration: '3:14'},
+        {title: 'Wrong phone number' , duration: '2:15'},
+    ]
+};
+
+var albumEinstein = {
+    title: 'Relativity',
+    artist: 'Albert Einstein',
+    label: 'Physics',
+    year: '1915',
+    albumArtUrl: 'assets/images/album_covers/19.png',
+    songs: [
+        {title: 'Geometry', duration: '1:21'},
+        {title: 'Tensors', duration: '3:54'},
+        {title: 'Calculus' , duration: '4:15'},
+        {title: 'Quasar' , duration: '1:15'},
+        {title: 'Black Hole' , duration: 'Infinity'},
+    ]
+};
