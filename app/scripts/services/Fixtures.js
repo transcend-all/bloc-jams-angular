10f6402dc52aed3272f54991e5d6c68d0fc48b40
transcend-all <transcendentalguy@gmail.com>
2016-12-15 16:58:42 -0500

    assignment 6 complete

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -34,10 +34,18 @@
         
         Fixtures.getAlbum = function(){
             return albumPicasso;
-        }
+        };
+        
+        Fixtures.getCollection = function(numberOfAlbums){
+            var albums = [];
+            for (var i=0;i<numberOfAlbums;i++){
+                albums.push(albumPicasso);
+            }
+            return albums;
+        };
         
         return Fixtures;
-    }
+    };
     
     angular
         .module('blocJams')
